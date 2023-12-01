082448d86e3c955feacdd9bb6d77e7ffd8bc19f4
“Vinicius <vinicius.a.cruz1@gmail.com>
2023-12-01 18:30:12 -0300

    Setting up files 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_flutter_web/screens/whole_page.dart';
 
 void main() {
   runApp(const MyApp());
---
+++ lib/screens/whole_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class WholePage extends StatefulWidget {
+  const WholePage({super.key});
+
+  @override
+  State<WholePage> createState() => _WholePageState();
+}
+
+class _WholePageState extends State<WholePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
